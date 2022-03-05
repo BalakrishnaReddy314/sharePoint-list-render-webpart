a2b3e11e5daf2773738a6e96fc8e43d7ca4ff5a1
Balakrishna Reddy <balakrishnareddy314@gmail.com>
2022-03-05 12:31:24 +0530

    initial commit

---
+++ src/webparts/renderList/RenderListWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'RenderListWebPartStrings';
+import RenderList from './components/RenderList';
+import { IRenderListProps } from './components/IRenderListProps';
+
+export interface IRenderListWebPartProps {
+  description: string;
+}
+
+export default class RenderListWebPart extends BaseClientSideWebPart<IRenderListWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IRenderListProps> = React.createElement(
+      RenderList,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/renderList/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IRenderListWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'RenderListWebPartStrings' {
+  const strings: IRenderListWebPartStrings;
+  export = strings;
+}
